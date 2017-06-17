f83fbf0b234ff7017819ebe4394f1528e6811856
Joel Spriggs <joel.spriggs@libertymutual.com>
2017-06-16 20:55:49 -0400

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { KeywordSearchComponent } from './keyword-search/keyword-search.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    KeywordSearchComponent
   ],
   imports: [
     BrowserModule
