2752d05f070b773a46267c379e4f5ccb1ac06c52
jrspriggs <joel.spriggs@gmail.com>
2017-06-16 21:47:10 -0400

    adding some modules for pulling in forms
    adding some modules for pulling in forms

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,9 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule, FormsModule, Validators } from '@angular/forms';
+import { StoreModule } from '@ngrx/store';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
 import { KeywordSearchComponent } from './keyword-search/keyword-search.component';
@@ -10,7 +14,10 @@ import { KeywordSearchComponent } from './keyword-search/keyword-search.componen
     KeywordSearchComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ReactiveFormsModule,
+    FormsModule,
+    HttpModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
